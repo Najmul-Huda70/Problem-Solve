7770c38dd3fd8c1a67258d1403fa686c4ade311c
Najmul Huda <nh5696080@gmail.com>
2024-10-28 13:50:50 +0600

    Add files via upload

---
+++ A_A_Gift_From_Orangutan.cpp
@@ -0,0 +1,46 @@
+/*************************************************************************
+ Don't feel alone. Allah is with you!
+
+ I said I failed...
+Allah says the believers are successful [Surat al-Mu'minun 23:1]
+I said none of my plans are succeeding.....
+Allah says Allah is the best planner [Surah Imran 3:54]
+ *************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using ll = long long;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vs = vector<string>;
+const int N=1e6+9;
+const ll mod = 1e5 + 7,inf = 1e9;
+int32_t main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
+ int t;cin>>t;
+ while(t--)
+ {
+  int n;
+  cin>>n;
+  vi v(n);
+  int mn=INT_MAX,mx=INT_MIN;
+  for(int i=0;i<n;i++)
+  {
+    cin>>v[i];
+    if(v[i]<mn) mn=v[i];
+    if(v[i]>mx) mx =v[i];
+   }
+  if(n==1) cout<<0<<endl;
+  else{
+        cout<<(mx-mn)*(n-1)<<endl;
+  }
+  
+  
+  
+ }
+return 0;
+}
