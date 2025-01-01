615c817d3f8fecc9a6e26202e8e6c4987416a7ff
Najmul Huda <nh5696080@gmail.com>
2025-01-01 20:32:39 +0600

    BST

---
+++ Year-2024/BST/Search_in_BST.cpp
@@ -0,0 +1,103 @@
+/****************************************************************************
+ Don't feel alone. Allah is with you!
+
+ I said I failed...
+Allah says the believers are successful [Surat al-Mu'minun 23:1]
+I said I have a lot of trouble in my life
+Allah says, Verily with hardship there is ease [Surah Alam Nasra 94:6]
+
+I said no one is helping me....
+Allah says it is my duty to help the believers [Surah Rum 30:47]
+
+I said no one with me.....
+Allah says fear not, I am with the believers [Surat al-Ha 20:46]
+I said my sins are many.....
+Allah says I love those who repent [Surah Baqarah 2:222]
+
+ I said I am always sick......
+Allah says: "I have sent the Qur'an as a cure for disease [Surah Bani-Israel 17:82]
+I said I don't like this world......
+Allah says the Hereafter is better for you than this life [Surah Duha 94:4]
+
+I said victory is far away....
+Allah says My help is near [Surah Baqarah 2:214]
+I said I am not happy in my life.....
+Allah says: Soon your Lord will give you so much that you will be happy [Surah Duha 93:5]
+
+I said I was disappointed all the time.....
+Allah says And do not despair and do not grieve [Surah Imran 3:139]
+I said none of my plans are succeeding.....
+Allah says Allah is the best planner [Surah Imran 3:54]
+ ****************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+int val;
+Node *left;
+Node* right;
+Node (int val)
+{
+    this->val=val;
+    this->left=this->right=NULL;
+}
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ano
+        Node *p = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // 3. children gulo ke push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+bool search(Node* root,int x)
+{
+    if(root==NULL) return false;
+    if(root->val==x) return true;
+    if(root->val>x) return search(root->left,x);
+    else return search(root->right,x);
+
+}   
+int main()
+{
+   Node* root =input_tree();
+   if(search(root,13)) cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+return 0;
+}
