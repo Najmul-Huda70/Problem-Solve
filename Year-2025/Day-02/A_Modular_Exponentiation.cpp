da7bc4df8e0dbb129fcc4a79720e5c6880bd60b0
Najmul Huda <nh5696080@gmail.com>
2025-01-03 22:28:56 +0600

    D2

---
+++ Year-2025/Day-02/A_Modular_Exponentiation.cpp
@@ -0,0 +1,26 @@
+// Najmul Huda
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using ll = long long;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vs = vector<string>;
+const int N = 1e6 + 9;
+const ll mod = 1e5 + 7, inf = 1e9;
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n, m;
+    cin >> n >> m;
+    if(n>27) cout<<m<<endl;
+    else {
+         cout<<m%((int)pow(2,n))<<endl;
+    }
+    
+    return 0;
+}
