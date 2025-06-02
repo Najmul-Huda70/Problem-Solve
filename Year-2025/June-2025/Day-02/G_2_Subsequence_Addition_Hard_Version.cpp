322b8b9cc46d3b4f8b5a0dc99c12c4b957621de7
Najmul Huda <nh5696080@gmail.com>
2025-06-02 17:51:43 +0300

    Add files via upload

---
+++ Year-2025/June-2025/Day-02/G_2_Subsequence_Addition_Hard_Version.cpp
@@ -0,0 +1,76 @@
+// Najmul Huda
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+#define SET(arr, a) memset(arr, a, sizeof(arr))
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define ROF(i, a, b) for (int i = a; i >= b; --i)
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define condition(flag) cout << (flag ? "YES" : "NO") << endl;
+#define vec_min(v) min_element(all(v));
+#define vec_max(v) max_element(all(v));
+#define vec_minmax(v) minmax_element(all(v));
+using ll = long long;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vs = vector<string>;
+const int N = 1e6 + 9;
+const ll mod = 1e5 + 7, inf = 1e9;
+template <typename Najmul>
+void print(const vector<Najmul> &ans)
+{
+    for (const auto val : ans)
+        cout << val << " ";
+    cout << endl;
+}
+/*
+ auto p = vec_minmax(a);
+        int mx_a = *p.second;
+        int mn_a = *p.first;
+*/
+void solve()
+{
+    int n, ans = 0;
+    cin >> n;
+
+    vector<int> arr(n + 1);
+
+    FOR(i, 1, n)
+    cin >> arr[i];
+    if (n == 1 && arr[1] != 1)
+    {
+        no;
+        return;
+    }
+    if (n == 1 && arr[1] == 1)
+    {
+        yes;
+        return;
+    }
+    stable_sort(all(arr));
+    ll sum = 1;
+    int i = 2;
+    while (i <= n && sum >= arr[i])
+    {
+        sum += 1LL * arr[i];
+        i++;
+    }
+    condition(i - 1 == n);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
