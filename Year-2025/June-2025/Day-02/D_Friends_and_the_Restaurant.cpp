352e88790b57d5be4900d5ef4e422f2c2f2fa53b
Najmul Huda <nh5696080@gmail.com>
2025-06-02 20:02:12 +0300

    Add files via upload

---
+++ Year-2025/June-2025/Day-02/D_Friends_and_the_Restaurant.cpp
@@ -0,0 +1,80 @@
+// Najmul Huda
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+#define SET(arr, a) memset(arr, a, sizeof(arr))
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define ROF(i, a, b) for (int i = a; i >= b; --i)
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define condition(flag) cout << (flag ? "YES" : "NO") << endl;
+#define vec_min(v) min_element(all(v));
+#define vec_max(v) max_element(all(v));
+#define vec_minmax(v) minmax_element(all(v));
+using ll = long long;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vs = vector<string>;
+const int N = 1e6 + 9;
+const ll mod = 1e5 + 7, inf = 1e9;
+template <typename Najmul>
+void print(const vector<Najmul> &ans)
+{
+    for (const auto val : ans)
+        cout << val << " ";
+    cout << endl;
+}
+/*
+ auto p = vec_minmax(a);
+        int mx_a = *p.second;
+        int mn_a = *p.first;
+*/
+void solve()
+{
+    int n, ans = 0;
+    cin >> n;
+
+    vi x(n), y(n);
+
+    FOR(i, 0, n - 1)
+    cin >> x[i];
+
+    FOR(i, 0, n - 1)
+    cin >> y[i];
+
+    vi compare(n, 0);
+    FOR(i, 0, n - 1)
+    {
+        compare[i] = y[i] - x[i];
+    }
+    stable_sort(all(compare));
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        if (-compare[i] <= compare[j])
+        {
+            ans++;
+            i++;
+            j--;
+        }
+        else
+            i++;
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
